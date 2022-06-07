1bfc2487f27c24b871dc8bce2c519e57fd38b0ce
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-06-06 21:25:36 -0300

    Implement the resolution of backend test questions

--- app/helpers/document.helper.js
+++ app/helpers/document.helper.js
@@ -1,18 +1,68 @@
 module.exports = {
-    wordFrequency: function(doc, word){
-        throw new Error("Not implemented exception")
-    },
-
-    wordSentences: function(doc, word){
-        throw new Error("Not implemented exception")
-    },
-
-    topWords: function(doc, count, minWordLength){
-        throw new Error("Not implemented exception")
-    },
-
-    stringToWordsArray: function(text) {
-        return text.match(/[a-zÀ-ú]+/gmui);
+  wordFrequency: function (doc, word) {
+    try {
+      return doc.match(new RegExp(word, "gimu")).length;
+    } catch (e) {
+      return e;
     }
-}
+  },
 
+  wordSentences: function (doc, word) {
+    const pattern = new RegExp("^[A-ZÀ-Ú][^(.:;\n)]+[(.:;\n)]", "gmu");
+
+    try {
+      return doc.match(pattern).filter((sentence) => {
+        return sentence.match(new RegExp(word, "gimu"));
+      });
+    } catch (e) {
+      return e;
+    }
+  },
+
+  topWords: function (doc, count, minWordLength) {
+    try {
+      const words = stringToWordsArray(doc.toLowerCase());
+      const wordCount = {};
+
+      words.forEach((word) => {
+        if (word.length >= minWordLength) {
+          if (wordCount[word]) {
+            wordCount[word]++;
+          } else {
+            wordCount[word] = 1;
+          }
+        }
+      });
+
+      const wordCountArray = Object.keys(wordCount).map((key) => {
+        return {
+          word: key,
+          count: wordCount[key],
+        };
+      });
+
+      let done = false;
+      while (!done) {
+        done = true;
+        for (let i = 0; i < wordCountArray.length; i++) {
+          for (let j = i + 1; j < wordCountArray.length; j++) {
+            if (wordCountArray[i].count < wordCountArray[j].count) {
+              done = false;
+              const aux = wordCountArray[i];
+              wordCountArray[i] = wordCountArray[j];
+              wordCountArray[j] = aux;
+            }
+          }
+        }
+      }
+
+      return wordCountArray.slice(0, count);
+    } catch (e) {
+      return e;
+    }
+  },
+
+  stringToWordsArray: function (text) {
+    return text.match(/[a-zÀ-ú]+/gimu);
+  },
+};
--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
-    username: 'root',
-    password: '123456',
-    database: 'netlex-test',
-    dialect: 'mysql',
-    port: '3306',
-    host: 'db-netlex-test'
-}
+  username: "root",
+  password: "123456",
+  database: "netlex-test",
+  dialect: "mysql",
+  port: "3306",
+  host: "db-netlex-test",
+};
